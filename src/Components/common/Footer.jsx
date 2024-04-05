bb9955ace4210242cddd621caa629f99754c2192
nikitaanagar <nikitanagar.1201@gmail.com>
2024-04-05 16:45:15 +0530

    Change Footer

--- src/Components/common/Footer.jsx
+++ src/Components/common/Footer.jsx
@@ -172,7 +172,7 @@ const Footer = () => {
             })}
           </div>
 
-          <div className="text-center">Made by Himanshu Sangwan 🗿 © 2023 Studynotion</div>
+          <div className="text-center">Made by Himanshu Sangwan and Nikita Nagar</div>
         </div>
       </div>
     </div>
